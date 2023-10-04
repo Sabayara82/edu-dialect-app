8c58b876f7878bb5671f17b5c35efcad891057ae
IshaHaider <66149883+IshaHaider@users.noreply.github.com>
2023-10-04 14:13:59 -0600

    add tab buttons

---
+++ src/components/containers/TabsContainer.jsx
@@ -0,0 +1,21 @@
+import { useNavigate } from "react-router-dom"
+
+export default function TabsContainer() {
+    const navigate = useNavigate();
+
+    return (
+        <>
+            <div className="flex h-full items-center">
+                <div className="flex justify-evenly h-[50%] w-full divide-x-2 border-2 border-solid">
+                    <button className="h-full" onClick={() => navigate("/about")}>About</button>
+                    <button className="pl-[8%] h-full" onClick={() => navigate("/impact")}>Impact</button>
+                    <button className="pl-[8%] h-full" onClick={() => navigate("/proficiencytest")}>Proficiency Test</button>
+                    <button className="pl-[8%] h-full" onClick={() => navigate("/support")}>Support</button>
+
+                </div>
+                
+
+            </div>
+        </>
+    );
+}
