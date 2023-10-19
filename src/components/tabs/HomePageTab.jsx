7b2756ff6461e59bea7b963a8e17cce4c152cff9
Saba <sabayara0@gmail.com>
2023-10-19 10:12:39 -0600

    homepage bug fixed

--- src/components/tabs/HomePageTab.jsx
+++ src/components/tabs/HomePageTab.jsx
@@ -2,10 +2,10 @@
 export default function HomePageTab() {
     return (
         //        <img className="w-32 md:w-48 lg:w-96"> <HomePageTab /> src="HomeImg.png"</img>
-        <div className="w-2/12 bg-white-700 flex justify-end items-center transform scale-90"><HomePageTab />
+        //<div className="w-2/12 bg-white-700 flex justify-end items-center transform scale-90"><HomePageTab />
             <img className="w-32 md:w-48 lg:w-96" src="HomeImg.png" />
         
-        </div>
+       // </div>
 
 
 
