92041826b9715a38b6e3370970c418bd34e7ade5
Saba <sabayara0@gmail.com>
2023-11-01 15:44:21 -0600

    Home page layout

--- src/components/tabs/HomePageTab.jsx
+++ src/components/tabs/HomePageTab.jsx
@@ -1,20 +1,27 @@
+import { Link } from "react-router-dom"; // Import the Link component
 
 export default function HomePageTab() {
-    return (
-        //        <img className="w-32 md:w-48 lg:w-96"> <HomePageTab /> src="HomeImg.png"</img>
-        //<div className="w-2/12 bg-white-700 flex justify-end items-center transform scale-90"><HomePageTab />
-        //Page container: 
-        <div className = "container" >
-            <div className = "left">
-                {/* welcome message   */}
-                <div className = "object-none object-left">WELCOME TO EDUDIALECT </div>
-                    <div className = "aboutButton"> </div>
-            </div>
-            <div className = "right">
-                <img className="object-none object-right-top bg-yellow-300  w-32 md:w-48 lg:w-96" src="HomeImg.png" />
-
-            </div>
-            <div/>
-        </div>
-    ); 
+  return (
+    // Full page container
+    <div className="h-screen bg-white flex">
+      {/* Left side of page */}
+      <div className="w-1/2 p-8 flex flex-col justify-center items-center">
+        <p className="text-6xl font-poppins font-bold mb-8">Welcome to EduDialect</p>
+        {/* "Learn More" button linking to the about page */}
+        <Link to="/about">
+          <button className="py-3 px-16 bg-[#719da3] font-bold text-white rounded-lg hover:bg-slate-100 hover:text-slate-900 mb-5">
+            Learn More
+          </button>
+        </Link>
+        {/* Links "Sign up" button to sign up page*/}
+        <button className="font-medium py-3 px-14 text-slate-700 border border-black rounded-lg hover:bg-slate-100 hover:text-slate-900">
+            Sign up
+          </button>
+      </div>
+      {/* Right side of the page */}
+      <div className="w-1/2 p-8 flex justify-center items-center">
+        <img src="HomeImg.png" alt="Image" className="max-w-full max-h-full" />
+      </div>
+    </div>
+  );
 }
