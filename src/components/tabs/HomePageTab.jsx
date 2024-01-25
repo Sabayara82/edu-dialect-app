c513d8baec74e68b07de807b2322057d4a71eb1b
Saba <sabayara0@gmail.com>
2024-01-25 14:21:08 -0700

    final homepage by me

--- src/components/tabs/HomePageTab.jsx
+++ src/components/tabs/HomePageTab.jsx
@@ -1,3 +1,6 @@
+
+
+
 import { Link } from "react-router-dom"; // Import the Link component
 
 export default function HomePageTab() {
@@ -5,23 +8,36 @@ export default function HomePageTab() {
     // Full page container
     <div className="h-screen bg-white flex flex-col sm:flex-row">
       {/* Left side of page */}
-      <div className="w-1/2 h-2/3 p-8 flex flex-col justify-center items-center">
-        <p className="text-6xl font-poppins font-bold mb-8">Welcome to EduDialect</p>
-        {/* "Learn More" button linking to the about page */}
-        <Link to="/about">
-          <button className="py-4 px-16 bg-[#719da3] font-bold text-white rounded-lg hover:bg-slate-100 hover:text-slate-900 mb-5">
-            Learn More
-          </button>
-        </Link>
-        {/* Links "Sign up" button to sign up page*/}
-        <button className="font-medium py-4 px-16 text-slate-700 border border-black rounded-lg hover:bg-slate-100 hover:text-slate-900">
-            Sign up
-          </button>
+      <div className="w-1/2 p-8 flex flex-col justify-center items-center">
+        <div className="flex-grow flex flex-col items-center justify-center mt-20">
+          <p className="text-6xl font-poppins font-bold max-w-full max-h-full mb-10 mt-20">Welcome to EduDialect</p>
+          <div className="flex-grow flex flex-col items-center "> {/* Adjust the mt-10 value */}
+          {/* "Learn More" button linking to the about page */}
+            <Link to="/about">
+                <button className="py-4 px-16 bg-[#719da3] font-bold text-white rounded-lg hover:bg-slate-100 hover:text-slate-900 mb-5">
+                  Learn More
+                </button>
+            </Link>
+            {/* Links "Sign up" button to sign up page*/}
+            <button className="font-medium py-4 px-16 text-slate-700 border border-black rounded-lg hover:bg-slate-100 hover:text-slate-900">
+              Sign up
+            </button>
+          </div>
+
+        </div>
       </div>
+
+
       {/* Right side of the page */}
-      <div className="w-1/2 h-2/3 p-8 flex justify-center items-center">
-        <img src="HomeImg.png" alt="Image" className="max-w-full max-h-full" />
+      <div className="w-1/2 p-8 flex flex-col justify-center items-center">
+        {/*  */}
+        <div className="flex-grow h-2/3 flex flex-col items-center justify-center">
+          <img src="HomeImg.png" alt="Image" className="max-w-full max-h-full mb-4" />
+        </div>
+        <div className="flex-grow">
+        </div>
       </div>
+
     </div>
   );
 }
